35d9ae604d3e2f5897b21e903db3e6b530a9b979
gonzal0lguin <golguinm@gmail.com>
2021-07-27 20:13:20 -0400

    Creation of minimax.py file.

---
+++ python_codes/minimax.py
@@ -0,0 +1,16 @@
+# this file compiles the minimax algorithm class
+
+from python_codes.grid import*
+
+bot = Tac()
+
+
+class miniMax(object):
+    def __init__(self):
+        pass
+
+    def best_move(self, pos):
+        pass
+
+    def minimax(self, pos, depth, isMaximizing):
+        pass
